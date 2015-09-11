6a973214b43e8553bcf2aaca7da590cf58c5e48b
Joshua La Pine <jlapine@cs.otago.ac.nz>
2015-09-11 18:44:03 +1200

    cons skyscrapers

--- Asgn1.py
+++ Asgn1.py
@@ -8,6 +8,12 @@ PLANE_COL = 11
 GRID = 2
 STREET = 1
 BLOCKS = [[0 for i in range(PLANE_ROW)] for j in range(PLANE_COL)]
+ZONE_FACTOR = (PLANE_ROW-1)/2
+
+def chance_selector(chance):
+    if random.uniform(0,1) < chance:
+        return True
+    return False
 
 def select_object(object):
     bpy.context.selected_objects.clear()
@@ -40,11 +46,37 @@ for i in range(0, PLANE_ROW):
         
         x = ((i-(PLANE_ROW/2)) * GRID) + (STREET * i)
         y = ((j-(PLANE_COL/2)) * GRID) + (STREET * j)
+
         verts = [[x, y, 0],[x, y + GRID, 0], [x + GRID, y + GRID, 0], [x + GRID, y, 0]]
         BLOCKS[i][j] = create_block('block' + str(i) + str(j), (0,0,0), verts)  
-       
+        
+
+bpy.ops.transform.translate(value = (-(PLANE_ROW-1)/2, -(PLANE_COL-1)/2, 0))
 
 
+for i in range(0, PLANE_ROW):
+    for j in range(0, PLANE_COL):
+        zone = max(math.fabs(i - ZONE_FACTOR) , math.fabs(j - ZONE_FACTOR))
+        cosIn = (math.pi/ZONE_FACTOR) * zone
+        chance = (math.cos(cosIn) + 1) / 2
+        
+        block_origin = BLOCKS[i][j].location
+        max_height = 10 - zone
+        if chance_selector(chance):
+            block = bpy.ops.mesh.primitive_cube_add(location = (block_origin.x, block_origin.y, 0))
+            select_object(block)
+            bpy.ops.transform.resize(value = (0.25, 0.25, random.uniform(max_height/2, max_height)))
+            block_ref = bpy.context.active_object
+            block_ref.location.z += block_ref.dimensions.z/2
+            
+'''
+bpy.context.selected_objects.clear()
+for i in range(0, PLANE_ROW):   
+    for j in range(0, PLANE_COL):
+        select_object(BLOCKS[i][j])
+        bpy.ops.transform.translate(-(PLANE_ROW - 1)/2, -(PLANE_COL - 1)/2, 0)
+   '''     
+'''
 for i in range (-5, 6):
     for j in range (-5, 6):
         block = bpy.ops.mesh.primitive_cube_add(location = (0 + i, 0 + j, 0))
@@ -52,5 +84,5 @@ for i in range (-5, 6):
         bpy.ops.transform.resize(value = (0.25, 0.25, random.uniform(0.5, 3)))
         block_ref = bpy.context.active_object
         block_ref.location.z += block_ref.dimensions.z/2
-        
+        '''
          
